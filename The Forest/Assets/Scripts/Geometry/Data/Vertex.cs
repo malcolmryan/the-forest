4cfde708bc8d3605af6a2219df5c56e22d06d086
Malcolm Ryan <malcoln.ryan@mq.edu.au>
2025-11-16 15:41:16 +1100

    Added shortcut v.x for v.position.x

--- The Forest/Assets/Scripts/Geometry/Data/Vertex.cs
+++ The Forest/Assets/Scripts/Geometry/Data/Vertex.cs
@@ -17,6 +17,9 @@ public class Vertex
     public HalfEdge edge;
     public string name;
 
+    public float x => position.x;
+    public float y => position.y;
+
     public Vertex(Vector2 position, string name)
     {
         this.position = position;
